8af29991affd95feb5394686052759be6d758c1b
Muhammad Rifqi Fatchurrahman <muh_rif@live.com>
2024-08-27 07:50:01 +0700

    remove old code

--- llm/impl/src/main/java/com/muhrifqii/llm/dto/CreateBedRequest.java
+++
@@ -1,7 +0,0 @@
-package com.muhrifqii.llm.dto;
-
-public record CreateBedRequest(
-                String name,
-                String description,
-                String category) {
-}
--- llm/impl/src/main/java/com/muhrifqii/llm/dto/ErrorHttpResponse.java
+++
@@ -1,7 +0,0 @@
-package com.muhrifqii.llm.dto;
-
-import lombok.Builder;
-
-@Builder
-public record ErrorHttpResponse(int status, int code, String message, String timestamp) {
-}
